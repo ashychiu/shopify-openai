2bb5c71df4bf2fccad0ebf11232d0f931e6c9833
Ashley Chiu <ashleychiu.codes@gmail.com>
2022-05-22 03:58:56 +0000

    Mapping prompts & responses

--- src/App.js
+++ src/App.js
@@ -5,72 +5,43 @@ import './App.scss';
 function App() {
 const [responses, setResponses] = useState([]);
 const [prompts, setPrompts] = useState([]);
-const [prompt, setPrompt] = useState([]);
+const [prompt, setPrompt] = useState({});
 
    useEffect(() => {
     setPrompts([...prompts, prompt])
-    const promptAPI = {
-      prompt: prompt,
-      // temperature: 0.5,
-      // max_tokens: 64,
-      // top_p: 1.0,
-      // frequency_penalty: 0.0,
-      // presence_penalty: 0.0,
-     };
  }, [prompt]);
 
- const promptAPI = {
-  prompt: prompt,
-  // temperature: 0.5,
-  // max_tokens: 64,
-  // top_p: 1.0,
-  // frequency_penalty: 0.0,
-  // presence_penalty: 0.0,
- };
-  
-//   useEffect(() => {
-//    const postPrompt = async () => {
-//     try {
-//       const res = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
-//           },
-//           body: JSON.stringify(promptAPI),
-//          })
-//          const data = await res.json();
-//          setResponses([...responses, data.choices[0]]);
-//          console.log("res", responses)
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-//   postPrompt();
-// }, []);
+const postPrompt = async () => {
+  try {
+    const res = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
+        },
+        body: JSON.stringify(prompt),
+       })
+       const data = await res.json();
+       setResponses([...responses, data.choices[0]]);
+       console.log("res", responses)
+  } catch (err) {
+    console.log(err);
+  }
+};
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const input = e.target[0].value;
-    setPrompt(input);
-    const postPrompt = async () => {
-      try {
-        const res = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
-            },
-            body: JSON.stringify(promptAPI),
-           })
-           const data = await res.json();
-           setResponses([...responses, data.choices[0]]);
-           console.log("res", responses)
-      } catch (err) {
-        console.log(err);
-      }
-    };
+    setPrompt({
+      prompt: input,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+     });
     postPrompt();
+    e.target.reset();
   };
 
   return (
@@ -90,20 +61,12 @@ const [prompt, setPrompt] = useState([]);
       <section>
         <h2>Responses</h2>
 <div className="test">
-        {prompts.map((prompt, index) => {
-        return (
-          <div key={index} className="response-card">
-            
-{prompt.prompt}
-          </div>
-        );
-      })}
-
         {responses.map((res, index) => {
+          const prompt = prompts[index];
         return (
           <div key={index} className="response-card">
-            
-{res.text}
+            <p>Prompt: {prompt.prompt}</p>    
+            <p>Response: {res.text}</p>
           </div>
         );
       })}
